8e3ef2bc3de39731021b3fe500572be275181318
nameless-cat1 <61926680+nameless-cat1@users.noreply.github.com>
2021-01-26 16:03:21 +0800

    Update plt5_ax_setting1.py
    加注释

--- matplotlibTUT/plt5_ax_setting1.py
+++ matplotlibTUT/plt5_ax_setting1.py
@@ -23,7 +23,7 @@ plt.plot(x, y2)
 # plot the second curve in this figure with certain parameters
 plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--')
 # set x limits
-plt.xlim((-1, 2))
+plt.xlim((-1, 2))     # 范围限定
 plt.ylim((-2, 3))
 plt.xlabel('I am x')
 plt.ylabel('I am y')
@@ -33,6 +33,10 @@ new_ticks = np.linspace(-1, 2, 5)
 print(new_ticks)
 plt.xticks(new_ticks)
 # set tick labels
+# 将坐标用其他替代
+# 用$$包括可以将字母变成斜体，注意空格需要反斜杠转义
+# 想打出罗马字母可用\alpha
 plt.yticks([-2, -1.8, -1, 1.22, 3],
            [r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
+
 plt.show()
