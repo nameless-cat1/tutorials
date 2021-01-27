6f061703d076cd469360bd25d5f356d57cea0d6e
nameless-cat1 <61926680+nameless-cat1@users.noreply.github.com>
2021-01-27 11:28:45 +0800

    Update plt7_legend.py
    加注释

--- matplotlibTUT/plt7_legend.py
+++ matplotlibTUT/plt7_legend.py
@@ -34,6 +34,7 @@ l1, = plt.plot(x, y1, label='linear line')
 l2, = plt.plot(x, y2, color='red', linewidth=1.0, linestyle='--', label='square line')
 
 plt.legend(loc='upper right')
+# 有这个才出现图例
 # plt.legend(handles=[l1, l2], labels=['up', 'down'],  loc='best')
 # the "," is very important in here l1, = plt... and l2, = plt... for this step
 """legend( handles=(line1, line2, line3),
